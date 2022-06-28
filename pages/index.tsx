2164592e43714a291b280bbf7c1f92978a0a8a7d
Umoren <samuelumoren365@gmail.com>
2022-06-28 21:06:42 +0100

    build: implemented home and about pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,28 @@
-import type { NextPage } from 'next'
-import CatCard from '../components/cards/cat/CatCard'
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks'
-import styles from '../styles/Home.module.css'
+import CatCard from '../components/cards/cat/CatCard';
+import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import styles from '../styles/Home.module.css';
+import { NextPageWithLayout } from './page';
 
-const Home: NextPage = () => {
+const Home: NextPageWithLayout = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <section className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <CatCard {...mockCatCardProps.base} />
+    </section>
+  );
+};
 
-        <div className={styles.grid}>
-          <CatCard {...mockCatCardProps.base} />
-          <CatCard {...mockCatCardProps.base} />
-          <CatCard {...mockCatCardProps.base} />
-          <CatCard {...mockCatCardProps.base} />
-        </div>
-      </main>
-    </div>
-  )
-}
+export default Home;
 
-export default Home
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout />
+      {page}
+    </PrimaryLayout>
+  );
+};
