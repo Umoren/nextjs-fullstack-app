0d1ba3b9e6dad879d429da0f8a58f981a1da871e
Umoren <samuelumoren365@gmail.com>
2022-06-29 12:07:32 +0100

    build: implemented google home ui

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -2,10 +2,23 @@ export interface ISearch {}
   
   const Search: React.FC<ISearch> = () => {
     return (
-      <form>
-        <input type="text" />
-        <button type="submit"> Google Search </button>
-        <button type="submit">I&apos;m Feeling Lucky</button>
+      <form className="flex flex-col items-center gap-y-5">
+        <input
+            type="text"
+            className="rounded-base border-2 p-3 w-5/6 sm:w-96"
+        />
+        <button
+            type="submit"
+            className="btn-primary"
+        >
+        Google Search
+        </button>
+        <button
+            type="submit"
+            className="btn-primary"
+        >
+            I&apos;m Feeling Lucky
+        </button>
       </form>
     );
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
   },
-  plugins: [],
 };
