461b58f6546c9f1753eb54e0c5ac6e8d6b4e5315
Umoren <samuelumoren365@gmail.com>
2022-06-29 20:12:34 +0100

    feat: connect search input to results page

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,41 +1,43 @@
+import { useRouter } from 'next/router';
 import { useState } from "react";
-export interface ISearch {}
-  
-  const Search: React.FC<ISearch> = () => {
+
+export interface ISearch { }
+
+const Search: React.FC<ISearch> = () => {
+    const router = useRouter();
     const [searchTerm, setSearchTerm] = useState<string>();
     return (
-      <form
-        className="flex flex-col items-center gap-y-5"
-        onSubmit={(e) => {
-            e.preventDefault();
-            alert(`Action requested. Search for term: ${searchTerm}`)
-        }}
+        <form
+            className="flex flex-col items-center gap-y-5"
+            onSubmit={(e) => {
+                e.preventDefault();
+                router.push(`/results?search=${searchTerm}`);
+            }}
 
         >
-        <input
-            type="text"
-            className="rounded-base border-2 p-3 w-5/6 sm:w-96"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div className="space-x-3">
-            <button
-                type="submit"
-                className="btn-primary"
-            >
-            Google Search
-            </button>
-            <button
-                type="submit"
-                className="btn-primary"
-            >
-                I&apos;m Feeling Lucky
-            </button>
-        </div>
+            <input
+                type="text"
+                className="rounded-base border-2 p-3 w-5/6 sm:w-96"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <div className="space-x-3">
+                <button
+                    type="submit"
+                    className="btn-primary"
+                >
+                    Google Search
+                </button>
+                <button
+                    type="submit"
+                    className="btn-primary"
+                >
+                    I&apos;m Feeling Lucky
+                </button>
+            </div>
 
-      </form>
+        </form>
     );
-  };
-  
-  export default Search;
-  
+};
+
+export default Search;
